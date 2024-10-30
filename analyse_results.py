17004e2c9ebba66815c10702bf15a004e4978538
Michele Garibbo <michele.garibbo@bristol.ac.uk>
2024-10-30 17:52:02 +0000

    plot

--- analyse_results.py
+++ analyse_results.py
@@ -4,17 +4,36 @@ import os
 from utils import protData_cleaning
 from transformers import GPT2Tokenizer
 import json
+import matplotlib.pyplot as plt
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-result_dir = os.path.join(root_dir,'results')
 
-# Compare perplexity for best 10% activity training sequences between base and dpo-fine-tuned protGPT2  
+# Training data
+trainingData_path = os.path.join(root_dir,'dataset','sequences.csv')
+training_data = pd.read_csv(trainingData_path)
+
+training_data = protData_cleaning(dataset=training_data, remove_activity_NaN=True)
+x = training_data['activity_dp7'].values
+
+# Compare dpo-FT model perplexity across training data
+result_dir = os.path.join(root_dir,'results')
 training_perplexities_file = os.path.join(result_dir,'TrainingDataPerplexities.json')
 training_perplexities = json.load(open(training_perplexities_file))
 
-print("base: ",training_perplexities['base_perplexity']) 
-print("sft: ",training_perplexities['sft_perplexity']) 
-print("dpo: ",training_perplexities['dpo_perplexity'], "\n") 
+y = training_perplexities['dpo_perplexity'] #NOTE: ensured the training data perplexities order reflects the original order
+
+plt.figure(figsize=(6, 6))
+plt.scatter(x, y, color='tab:blue',alpha=0.8)
+plt.xlabel("actvity_dp7")
+plt.ylabel("Perxplexity")
+plt.title("dpo-FT protGPT2 perplexity vs activity")
+ax = plt.gca()  # Get current axis
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.show()
+
+print(len(dpo_training_perplexities))
+print(len(activities))
 exit()
 
 # Load data
@@ -37,6 +56,3 @@ for d,e in zip(dpo_seq['mutated_sequence'], base_seq['mutated_sequence']):
     print(e, "\n")
     exit()
 
-# Training data
-trainingData_path = os.path.join(root_dir,'dataset','sequences.csv')
-training_data = pd.read_csv(trainingData_path)
